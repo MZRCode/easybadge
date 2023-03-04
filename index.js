cbf0033f8ab7a7dfe9132bb65d5eaf77f13ae4c2
MZR Code <87870017+MZRCode@users.noreply.github.com>
2023-03-04 22:03:41 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+const { REST, Routes, Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+const TOKEN = process.env.token;
+const BOT_CLIENT_ID = "";
+
+const commands = [
+  {
+    name: 'selam',
+    description: 'Size Aleyküm Selam der',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+(async () => {
+  try {
+    console.log('/ komutları yeniden yükleniyor!');
+
+    await rest.put(Routes.applicationCommands(BOT_CLIENT_ID), { body: commands });
+
+    console.log('/ komutları başarıyla yeniden yüklendi!');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+client.on('ready', () => {
+  console.log(`${client.user.tag} olarak giriş yaptı!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'selam') {
+    await interaction.reply('Aleyküm Selam');
+  }
+});
+
+client.login(TOKEN);
